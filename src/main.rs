b5983c983af417ae9fb4ca15d9114378499082be
bynect <68197565+bynect@users.noreply.github.com>
2021-12-03 15:34:00 +0100

    Add directives structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod dir;
 mod expr;
 mod parse;
 mod split;
@@ -7,68 +8,9 @@ use expr::Expr;
 use std::io::{self, Write};
 use sub::Subst;
 
-struct Config {
+pub struct Config {
     limit: usize,
     subst: Subst<Expr>,
-}
-
-struct Directive {
-    arity: Option<u8>,
-    fun: fn(&mut Config, &str, &str),
-}
-
-fn directive(dir: &[Directive], cfg: &mut Config, line: &str) {
-    let buf = split::split_n_whitespace(line, 2).collect::<Vec<_>>();
-    if buf.is_empty() {
-        println!("");
-    } else {
-        match buf[0].slice {
-            "limit" => {
-                if buf.len() == 2 {
-                    let mut it = split::split_n_whitespace(buf[1].slice, 2);
-                    if let Some(first) = it.next() {
-                        if let Some(rest) = it.next() {
-                            eprintln!("Unexpected trailing characters `{}`", rest.slice);
-                            return;
-                        }
-
-                        match first.slice.parse::<usize>() {
-                            Ok(n) => cfg.limit = n,
-                            Err(e) => eprintln!("Error: {}", e),
-                        }
-                    } else {
-                        println!("limit = {}", cfg.limit)
-                    }
-                } else {
-                    println!("limit = {}", cfg.limit)
-                }
-            }
-            "let" => {
-                if buf.len() == 2 {
-                    let mut it = split::split_n_whitespace(buf[1].slice, 2);
-                    if let Some(x) = it.next() {
-                        if let Some(e) = it.next() {
-                            debug_assert!(it.next().is_none());
-                            match parse::parse(e.slice) {
-                                Some(Ok(e)) => {
-                                    cfg.subst = cfg.subst.extend(x.slice.to_string(), e);
-                                }
-                                Some(Err(e)) => eprintln!("{}", e),
-                                None => eprintln!("Expected expression"),
-                            };
-                        } else {
-                            eprintln!("Expected expression")
-                        }
-                    } else {
-                        eprintln!("Expected 2 arguments to directive let")
-                    }
-                } else {
-                    eprintln!("Expected 2 arguments to directive let")
-                }
-            }
-            _ => eprintln!("Unrecognized directive {}", buf[0].slice),
-        }
-    }
 }
 
 fn main() -> Result<(), io::Error> {
@@ -91,6 +33,8 @@ fn main() -> Result<(), io::Error> {
         limit: 100usize,
     };
 
+    let dirs = [dir::LIMIT_DIRECTIVE, dir::LET_DIRECTIVE];
+
     let mut line = String::new();
     loop {
         print!(">> ");
@@ -99,7 +43,7 @@ fn main() -> Result<(), io::Error> {
 
         // Directives (prefixed by :) or expressions
         if let Some(line) = line.strip_prefix(':') {
-            directive(&mut cfg, line);
+            dir::directive(&dirs, &mut cfg, line);
         } else {
             match parse::parse(&line) {
                 Some(Ok(mut e)) => {
